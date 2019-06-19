914dfcbf9c491be7dd62182e7198903ee5020f79
Magdalena <magdalenaj47@gmail.com>
2019-06-19 13:08:08 +0200

    cos tam

--- the_game.py
+++ the_game.py
@@ -2,7 +2,7 @@ from numpy import random
 
 
 def get_result():
-	possible_results = ["You loose!", "You win!"]
+	possible_results = ["You lose!", "You win!"]
 	chances = [0.8, 0.2]
 	return random.choice(possible_results, p=chances)
 
