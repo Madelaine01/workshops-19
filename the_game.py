034f903d4ec22dadde55cb95c73d6945e3cbbf4f
Magdalena <magdalenaj47@gmail.com>
2019-06-19 12:48:58 +0200

    Initial commit

---
+++ the_game.py
@@ -0,0 +1,16 @@
+from numpy import random
+
+
+def get_result():
+	possible_results = ["You loose!", "You win!"]
+	chances = [0.8, 0.2]
+	return random.choice(possible_results, p=chances)
+
+
+def main():
+	input("Wellcome! Enter your name: ")
+	print(get_result())
+
+
+if __name__ == '__main__':
+	main()
